fec9cd9f97865dc613ad40abe2249b472861ac91
yurany <yuranyguev@unicauca.edu.co>
2024-01-20 12:16:43 -0500

    integrante semillero validator

---
+++ src/main/java/edu/unicauca/SivriBackendApp/core/semillero/dominio/validadores/IntegranteSemilleroValidador.java
@@ -0,0 +1,12 @@
+package edu.unicauca.SivriBackendApp.core.semillero.dominio.validadores;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@AllArgsConstructor
+public class IntegranteSemilleroValidador {
+    //el estado de un integrante es inactivo
+
+}
+
