737454ac6cb5937833e333f398e114a1f01643d2
LAPTOP-RUETC5JV\mandr <mandresmosquera@gmail.com>
2024-10-09 09:19:20 -0600

    Listos para continuar

--- src/main/java/edu/unicauca/SivriBackendApp/common/email/service/SendMessageService.java
+++ src/main/java/edu/unicauca/SivriBackendApp/common/email/service/SendMessageService.java
@@ -111,7 +111,6 @@ public class SendMessageService {
 	}
 
 	private Boolean validarCorreo(String email) {
-		System.out.println("-> Coreo " + email);
 		Pattern pattern = Pattern
 				.compile("^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$");
 		Matcher mather = pattern.matcher(email);
