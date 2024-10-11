e0b60547650c6ffb9da33586e0c133c28041aeff
yuranyguev <yuranyguev@unicauca.edu.co>
2024-10-10 21:13:34 -0500

    correcion query de listar integrantes por patron

--- src/main/java/edu/unicauca/SivriBackendApp/core/semillero/infraestructura/adaptadores/out/persistencia/repository/IIntegranteSemilleroRepository.java
+++ src/main/java/edu/unicauca/SivriBackendApp/core/semillero/infraestructura/adaptadores/out/persistencia/repository/IIntegranteSemilleroRepository.java
@@ -55,27 +55,26 @@ public interface IIntegranteSemilleroRepository extends JpaRepository<Integrante
             @PageableDefault(size = 10,page = 0,sort = "id") Pageable pageable);
 
     //query ok
-    @Query(value = "SELECT  " +
-            "                u.numero_Documento, " +
-            "                u.nombre|| ' '|| u.apellido AS nombres, " +
-            "                s.semillero_Id, " +
-            "                odi.nombre AS nombreSemillero, " +
-            "                rs.rol_Semillero, " +
-            "                is2.estado, " +
-            "                u.programa_Id " +
-            "            FROM " +
-            "                integrante_semillero is2 " +
-            "                INNER JOIN rol_semillero rs ON rs.id = is2.rol_Id " +
-            "                INNER JOIN semillero s ON s.semillero_Id = is2.semillero_Id " +
-            "                INNER JOIN organismo_de_investigacion odi ON odi.id = s.semillero_Id " +
-            "                INNER JOIN usuario u ON u.id = is2.usuario_Id " +
-            "            WHERE " +
-            "                (LOWER(u.numero_Documento) LIKE '%'|| LOWER(:numeroDocumento)||'%') OR (:numeroDocumento IS NULL) OR (:numeroDocumento = '') " +
-            "                AND (LOWER(u.nombre|| ' '|| u.apellido) LIKE '%'|| LOWER(:nombres)|| '%' OR :nombres IS NULL OR :nombres = '') " +
-            "                AND (s.semillero_Id LIKE '%'|| :semilleroId|| '%' OR :semilleroId IS NULL OR :semilleroId = '') " +
-            "                AND (LOWER(odi.nombre) LIKE '%'|| LOWER(:nombreSemillero)|| '%' OR :nombreSemillero IS NULL OR :nombreSemillero = '') " +
-            "                AND (LOWER(rs.rol_Semillero) = LOWER(:rolSemillero) OR :rolSemillero IS NULL OR :rolSemillero = '') " +
-            "                AND (LOWER(is2.estado) = LOWER(:estado) OR :estado IS NULL OR :estado = '')",nativeQuery = true)
+    @Query(value = "SELECT u.numero_Documento, " +
+            "      u.nombre|| ' '|| u.apellido AS nombres, " +
+            "      s.semillero_Id, " +
+            "      odi.nombre AS nombreSemillero, " +
+            "      rs.ID, " +
+            "      rs.rol_Semillero, " +
+            "      is2.estado, " +
+            "      u.programa_Id " +
+            "    FROM integrante_semillero is2 " +
+            "    INNER JOIN rol_semillero rs ON rs.id = is2.rol_Id " +
+            "    INNER JOIN semillero s ON s.semillero_Id = is2.semillero_Id " +
+            "    INNER JOIN organismo_de_investigacion odi ON odi.id = s.semillero_Id " +
+            "    INNER JOIN usuario u ON u.id = is2.usuario_Id " +
+            "    WHERE " +
+            "      ((LOWER(u.numero_Documento) LIKE '%'|| LOWER(:numeroDocumento)||'%') OR (:numeroDocumento IS NULL) OR (:numeroDocumento = '')) " +
+            "      AND ((LOWER(u.nombre|| ' '|| u.apellido) LIKE '%' || LOWER(:nombres) || '%') OR (:nombres IS NULL) OR (:nombres = '')) " +
+            "      AND ((s.semillero_Id LIKE '%'|| :semilleroId|| '%') OR (:semilleroId IS NULL) OR (:semilleroId = '')) " +
+            "      AND ((LOWER(odi.nombre) LIKE '%'|| LOWER(:nombreSemillero)|| '%') OR (:nombreSemillero IS NULL) OR (:nombreSemillero = '')) " +
+            "      AND ((LOWER(rs.ID) = LOWER(:rolSemillero)) OR (:rolSemillero IS NULL) OR (:rolSemillero = '')) " +
+            "      AND ((LOWER(is2.estado) = LOWER(:estado)) OR (:estado IS NULL) OR (:estado = ''))",nativeQuery = true)
     Page<List<ListarTodosIntegrantesConFiltro>> listarTodosIntegranteSemilleroConFiltro(
             @Param("numeroDocumento") String numeroDocumento,
             @Param("nombres") String nombres,
